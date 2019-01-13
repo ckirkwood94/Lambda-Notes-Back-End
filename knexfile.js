d6714bf3f42981bfd745c6ada7e4ac49b726b138
ckirkwood94 <ckirkwood94@gmail.com>
2019-01-12 20:47:45 -0500

    Changed production vars in knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,4 +1,12 @@
 // Update with your config settings.
+require('dotenv').config();
+const localPg = {
+  host: 'localhost',
+  database: 'lambda',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+};
+const dbConnection = process.env.DATABASE_URL || localPg;
 
 module.exports = {
   development: {
@@ -34,17 +42,16 @@ module.exports = {
 
   production: {
     client: 'pg',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password',
-    },
+    connection: dbConnection,
     pool: {
       min: 2,
       max: 10,
     },
     migrations: {
-      tableName: 'knex_migrations',
+      tableName: './data/migrations',
+    },
+    seeds: {
+      directory: './data/seeds',
     },
   },
 };
